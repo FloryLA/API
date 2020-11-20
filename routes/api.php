7aba3d29c3b9ab408a5d824e118b0067a3f834df
FloryLA <sasukefmt93@gmail.com>
2020-11-20 11:03:29 -0600

    rutas
    Modificacion : de la ruta proyectos y catagerias

--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
 Route::get('/proyectos', 'ProjectController@index');
+Route::resource('categorias', 'CategoryController');
+Route::resource('proyectos', 'ProjectController');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
