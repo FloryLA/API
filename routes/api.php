390d8e809052ec634417cf7b3a1e080c299a0a78
FloryLA <sasukefmt93@gmail.com>
2020-12-11 12:05:58 -0600

    Update api.php
    Actualizacion de rutas API.PHP

--- routes/api.php
+++ routes/api.php
@@ -2,43 +2,33 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+/*
 DB::listen(function($query){
     //Imprimimos la consulta ejecutada
     echo "<pre> {$query->sql } </pre>";
-  });
+  });*/
   
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+
 
 
 Route::resource('proyectos', 'ProjectController');
-//Route::resource('estados', 'StateController');
-//Route::post("agenda", "AgendaController@store");
 Route::resource('zonashorarias', 'TimezoneController');
 
+//Route::resource('estados', 'StateController');
+//Route::post("agenda", "AgendaController@store");
 //Route::post("agenda/proyecto", "AgendaController@proyecto");
 
-//Eventos
+//===============Eventos========
 Route::post("eventos/dia","EventController@getEvents");
 Route::post("eventos/usuario","EventController@Eventsusuario");
 //Route::post("eventos/mes","EventController@getEventsmes");
 Route::resource('eventos', 'EventController');
 Route::post("eventos/proyecto", "EventController@proyecto");
 
+//================
 //Route::resource('eventos.proyecto', 'Event\EventProjectController',['only'=>['index']]);
-
-//Proyectos
 //Route::resource('proyectos.eventos', 'ProjectEventController',['except'=>['show']]);
-
 //Route::resource('categorias', 'CategoryController',['only'=>['index','show']]);
 //Route::resource('proyectos.categorias', 'ProjectCategoryController',['except'=>['show']]);
 
